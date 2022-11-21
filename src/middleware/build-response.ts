e1b55cfd9a233ec8f3124f5bc4fa025d212816a0
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2022-11-21 19:44:28 +0700

    feat: add build response middleware

--- src/middleware/build-response.ts
+++ src/middleware/build-response.ts
@@ -1,7 +1,14 @@
 import { Response } from 'express';
+import * as HttpStatus from 'http-status-codes';
 
-const buildResponse = (res: Response) => {
-    return (status: number, data: any) => {
-        res.status(status).send(data);
-    };
+export const buildResponse = (res: Response, httpStatus: number , data: any) => {
+    let statusText = HttpStatus.getStatusText(httpStatus).toUpperCase();
+    statusText = statusText.replace(/ /g, '_');
+
+    const resData = {
+        status: statusText,
+        data: data? data : null
+    }
+    res.status(httpStatus).send(resData);
 };
+
