9885095099af366bbcd26536820413df3a258648
arikrayi <arikrayi@gmail.com>
2022-11-22 00:10:56 +0700

    fix: CORS blocking

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from './infrastructure/http/route';
 
@@ -7,6 +8,20 @@ class App {
 
     constructor() {
         this.server = express();
+        const whitelist = [process.env.BINOTIFY_PREMIUM_FRONTEND_URL]
+        const corsOptions = {
+            origin: function (origin: string | undefined, callback: (arg0: Error | null, arg1: boolean | undefined) => void) {
+                if (!origin) {
+                    return callback(null, true);
+                }
+                if (whitelist.indexOf(origin) !== -1) {
+                    callback(null, true);
+                } else {
+                    callback(new Error('Not allowed by CORS'), undefined)
+                }
+            }
+        }
+        this.server.use(cors(corsOptions));
 
         this.middlewares();
         this.routes();
