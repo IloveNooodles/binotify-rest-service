97002dde7bf6afaab431882edcfb84d13b71d0e0
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2022-11-28 20:56:18 +0700

    feat: add video upload client interface

---
+++ src/interface/client/audio.ts
@@ -0,0 +1,14 @@
+import BinotifyPhpService from "../../infrastructure/client/binotify-php-service";
+
+const postAudio = async (audio_file: any) => {
+    const url = '/song/new_premium'
+
+    const formData = new FormData();
+    formData.append('song', audio_file);
+
+    const response = await BinotifyPhpService.post(url, formData);
+
+    return response;
+}
+
+export { postAudio };
