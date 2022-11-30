1e4582e7b6ef8b9fc05e900d3320473470376296
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2022-11-30 19:19:20 +0700

    fix: delete unused method on subscription service

--- src/infrastructure/client/subscription-service.ts
+++ src/infrastructure/client/subscription-service.ts
@@ -3,25 +3,17 @@ const SUBSCRIPTION_SERVICE_BASE_URL = process.env
 
 export default class SubscriptionService {
     public static async post(path: string, data: any): Promise<any> {
-        const response = await fetch(SUBSCRIPTION_SERVICE_BASE_URL + path, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/xml'
-            },
-            body: data
-        });
-
-        return response.json();
-    }
-
-    public static async get(path: string): Promise<any> {
-        const response = await fetch(SUBSCRIPTION_SERVICE_BASE_URL + path, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/xml'
+        const response: any = await fetch(
+            SUBSCRIPTION_SERVICE_BASE_URL + path,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'text/xml'
+                },
+                body: data
             }
-        });
+        );
 
-        return response.json();
+        return response;
     }
 }
